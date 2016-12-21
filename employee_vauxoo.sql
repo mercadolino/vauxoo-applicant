05f4342fce0fadee1088f15baf67a963dfc00e26
mercadolinomx <mercadolinomx@gmail.com>
2016-12-20 21:31:55 -0600

    submission sql2

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -13,9 +13,29 @@ CREATE TABLE employee(
 );
 
 CREATE TABLE employee_department(
-  id SERIAL  PRIMARY KEY,
+  id SERIAL PRIMARY KEY,
   name VARCHAR (50) UNIQUE NOT NULL,
   descr text
+);
+
+CREATE TABLE employee_hobby(
+
+  id SERIAL PRIMARY KEY,
+  name VARCHAR (50) UNIQUE NOT NULL,
+  descr text
+);
+
+CREATE TABLE employee_hobby_ref(
+
+  id_employee SERIAL,
+  id_employee_hobby SERIAL,
+
+  FOREIGN KEY (id_employee) REFERENCES employee
+  ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (id_employee_hobby) REFERENCES employee_hobby
+  ON DELETE CASCADE ON UPDATE CASCADE,
+
+  PRIMARY KEY (id_employee, id_employee_hobby)
 );
 
 INSERT INTO employee_department (name,descr) VALUES ('HR', 'descr1');
@@ -29,3 +49,16 @@ INSERT INTO employee (first_name, last_name, id_department) VALUES ('Demian', 'R
 INSERT INTO employee (first_name, last_name, id_department) VALUES ('Francesco', 'Apagno','1');
 INSERT INTO employee (first_name, last_name, id_department) VALUES ('Mark', 'Baumann','2');
 INSERT INTO employee (first_name, last_name, id_department) VALUES ('Francesco', 'Prova','3');
+
+INSERT INTO employee_hobby (name,descr) VALUES ('Photography', 'Love to take pictures');
+INSERT INTO employee_hobby (name,descr) VALUES ('Astronomy', 'Love to observe the night sky');
+INSERT INTO employee_hobby (name,descr) VALUES ('Hiking', 'Love to climb mountains');
+
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('1','1');
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('1','2');
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('2','1');
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('2','2');
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('3','1');
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('3','2');
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('4','1');
+INSERT INTO employee_hobby_ref (id_employee,id_employee_hobby) VALUES ('4','2');
