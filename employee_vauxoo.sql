4507033e5d889cf7541b1fdb44a688b927964609
mercadolinomx <mercadolinomx@gmail.com>
2016-12-20 21:54:28 -0600

    submission sql3

--- employee_vauxoo.sql
+++ employee_vauxoo.sql
@@ -7,9 +7,12 @@ CREATE TABLE employee(
   id serial PRIMARY KEY,
   first_name VARCHAR (50) NOT NULL,
   last_name VARCHAR (50) NOT NULL,
-  id_department integer NOT NULL,
+  id_department INTEGER NOT NULL,
+  id_boss INTEGER NOT NULL,
   FOREIGN KEY (id_department) REFERENCES employee_department
-      ON DELETE CASCADE ON UPDATE CASCADE
+      ON DELETE CASCADE ON UPDATE CASCADE,
+  FOREIGN KEY (id_boss) REFERENCES employee
+      ON DELETE CASCADE ON UPDATE CASCADE,
 );
 
 CREATE TABLE employee_department(
@@ -45,10 +48,10 @@ INSERT INTO employee_department (name,descr) VALUES ('Acquisti', 'descr4');
 INSERT INTO employee_department (name,descr) VALUES ('Vendite', 'descr5');
 INSERT INTO employee_department (name,descr) VALUES ('Marketing', 'descr6');
 
-INSERT INTO employee (first_name, last_name, id_department) VALUES ('Demian', 'Rihs','4');
-INSERT INTO employee (first_name, last_name, id_department) VALUES ('Francesco', 'Apagno','1');
-INSERT INTO employee (first_name, last_name, id_department) VALUES ('Mark', 'Baumann','2');
-INSERT INTO employee (first_name, last_name, id_department) VALUES ('Francesco', 'Prova','3');
+INSERT INTO employee (first_name, last_name, id_department, id_boss) VALUES ('Demian', 'Rihs','4','2');
+INSERT INTO employee (first_name, last_name, id_department, id_boss) VALUES ('Francesco', 'Apagno','1','2');
+INSERT INTO employee (first_name, last_name, id_department, id_boss) VALUES ('Mark', 'Baumann','2','2');
+INSERT INTO employee (first_name, last_name, id_department, id_boss) VALUES ('Francesco', 'Prova','3','2');
 
 INSERT INTO employee_hobby (name,descr) VALUES ('Photography', 'Love to take pictures');
 INSERT INTO employee_hobby (name,descr) VALUES ('Astronomy', 'Love to observe the night sky');
