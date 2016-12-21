8c84926c3d935600acf4246d261761a246c2a30c
mercadolinomx <mercadolinomx@gmail.com>
2016-12-20 18:36:38 -0600

    first commitment of primes.py

--- primes.py
+++ primes.py
@@ -1,16 +1,35 @@
+
 """
-Your module documentation here
+Controlor de numeros primos de Demian Rihs
 """
+from math import sqrt
+from itertools import count, islice
+
+__version__ = '1.0'
+__author__ = 'Demian Rihs'
 
 
 class PrimeClass(object):
     """
-    Your class documentation here
+    Esta classe genera un objecto con un metodo is_prime()
+    que retorna True cuando la variabile tiene un nr primo
     """
+
+    def _init_(self, num_int):
+        self.num_int = num_int
 
     def is_prime(self, num_int):
         """
-        Your method documentation here
+        itertools tiene una manera eficiente de utilizar rangos
+        de numeros y no se riesga un error de Overlow si
+        el numero esta mas grande que 2^31.
+        Para ahorrar tiempo de calculo es suficiente calcular
+        los numeros entre num_int y sqrt(num_int).
         """
-        # your primes code here
-        return "not implement yet"  # Remove this dummy line
+
+        if num_int < 2:
+            return False
+        for el in islice(count(2), int(sqrt(num_int))):
+            if not num_int % el:
+                return False
+        return True
